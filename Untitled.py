6d1b32f4ed1db508f1224b40b70d46a8f928b3d0
PrithikaArul <67939834+PrithikaArul@users.noreply.github.com>
2020-07-22 10:35:20 +0530

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,236 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello python")
+
+
+# In[2]:
+
+
+print("hellopython")
+
+
+# In[4]:
+
+
+print("welcome to python's world")
+
+
+# In[12]:
+
+
+print("prithika\'s world")
+
+
+# In[9]:
+
+
+print("hello\npython")
+
+
+# In[10]:
+
+
+print("hello\tpython")
+
+
+# In[14]:
+
+
+name="Prithika"
+marks="98.278"
+age="17"
+print("My name is",name,"I have scored",marks,"My age is",age)
+
+
+# In[21]:
+
+
+x="10"
+y="20"
+print(x)
+
+
+# In[22]:
+
+
+print(y)
+
+
+# In[23]:
+
+
+print(id(x))
+
+
+# In[24]:
+
+
+print(id(y))
+
+
+# In[26]:
+
+
+print(20/10)
+
+
+# In[27]:
+
+
+print(20+10)
+
+
+# In[28]:
+
+
+print(20-10)
+
+
+# In[29]:
+
+
+print(20*10)
+
+
+# In[30]:
+
+
+print(20//10)
+
+
+# In[31]:
+
+
+print(20//3)
+
+
+# In[32]:
+
+
+print(20%3)
+
+
+# In[33]:
+
+
+a=10
+b=20
+a==b
+
+
+# In[34]:
+
+
+a!=b
+
+
+# In[35]:
+
+
+a<b
+
+
+# In[36]:
+
+
+a>b
+
+
+# In[37]:
+
+
+a<=b
+
+
+# In[38]:
+
+
+a>=b
+
+
+# In[39]:
+
+
+a=200
+b=3
+a|b
+
+
+# In[40]:
+
+
+bin(a)
+
+
+# In[41]:
+
+
+a&b
+
+
+# In[42]:
+
+
+a^b
+
+
+# In[45]:
+
+
+20>10 and 30<M
+
+
+# In[46]:
+
+
+20<10 and 30<M
+
+
+# In[50]:
+
+
+str="computing"
+"u" in str
+
+
+# In[51]:
+
+
+"a" in str
+
+
+# In[52]:
+
+
+a=10
+b=10
+a is b
+
+
+# In[53]:
+
+
+a is not b
+
+
+# In[54]:
+
+
+20+10*9/2
+
+
+# In[56]:
+
+
+6**-1
+
+
+# In[ ]:
+
+
+
+
